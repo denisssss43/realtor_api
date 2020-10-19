f2c24fbc6272b5cface251adbfc2565d1ced13ef
Denis <38821830+denisssss43@users.noreply.github.com>
2020-10-19 12:22:15 +0700

    Update 20201019042207_create_resource_types.rb

--- db/migrate/20201019042207_create_resource_types.rb
+++ db/migrate/20201019042207_create_resource_types.rb
@@ -1,7 +1,7 @@
 class CreateResourceTypes < ActiveRecord::Migration[6.0]
   def change
     create_table :resource_types do |t|
-      t.string :title
+      t.string :title, null: false, unique: true
 
       t.timestamps
     end
