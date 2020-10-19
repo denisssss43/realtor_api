361bbbbcc644381afb60e93fe50f1a8f878fc71f
Denis <38821830+denisssss43@users.noreply.github.com>
2020-10-19 12:21:41 +0700

    Update offer.rb

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,19 @@
 class Offer < ApplicationRecord
   belongs_to :resource
   belongs_to :address
+
+  enum rental_period: {month:0, day:1}
+  enum sidekiq_status: {searcher:0, parser:1, completed:2, error:3}
+
+  has_many :offer_telephone_numbers
+  has_many :telephone_numbers, through: :offer_telephone_numbers
+
+  has_many :offer_hashtags
+  has_many :hashtags, through: :offer_hashtags
+  
+  has_many :offer_urls
+  has_many :urls, through: :offer_urls
+  
+  has_many :offer_img_urls
+  has_many :img_urls, through: :offer_img_urls
 end
