83487bc0534b2394c2b47f43f089904d09887885
Denis <38821830+denisssss43@users.noreply.github.com>
2020-10-19 12:22:07 +0700

    Update 20201019042149_create_urls.rb

--- db/migrate/20201019042149_create_urls.rb
+++ db/migrate/20201019042149_create_urls.rb
@@ -1,7 +1,7 @@
 class CreateUrls < ActiveRecord::Migration[6.0]
   def change
     create_table :urls do |t|
-      t.string :value
+      t.string :value, null: false, unique: true
 
       t.timestamps
     end
