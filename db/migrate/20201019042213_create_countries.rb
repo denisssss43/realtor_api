4a32cc6375c780f4d2432f187ae2968ed5b4b489
Denis <38821830+denisssss43@users.noreply.github.com>
2020-10-19 12:22:19 +0700

    Update 20201019042213_create_countries.rb

--- db/migrate/20201019042213_create_countries.rb
+++ db/migrate/20201019042213_create_countries.rb
@@ -1,7 +1,8 @@
 class CreateCountries < ActiveRecord::Migration[6.0]
   def change
     create_table :countries do |t|
-      t.string :title
+      t.references :country, null: false, foreign_key: true
+      t.string :title, null: false
 
       t.timestamps
     end
