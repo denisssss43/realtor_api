1b207afe1620b3fa4693e8625eecc22d35714034
Denis <38821830+denisssss43@users.noreply.github.com>
2020-10-19 12:22:32 +0700

    Update 20201019042309_create_offers.rb

--- db/migrate/20201019042309_create_offers.rb
+++ db/migrate/20201019042309_create_offers.rb
@@ -3,16 +3,16 @@ class CreateOffers < ActiveRecord::Migration[6.0]
     create_table :offers do |t|
       t.references :resource, null: false, foreign_key: true
       t.references :address, null: false, foreign_key: true
-      t.decimal :price, precision: 15, scale: 2
+      t.decimal :price, precision: 15, scale: 2, null: false
       t.int :square_meters
       t.int :room_count
       t.int :floor_number
       t.int :apartment_number
       t.boolean :is_furniture
-      t.boolean :is_realtor
-      t.int :rental_period
-      t.text :description
-      t.int :sidekiq_status
+      t.boolean :is_realtor, default: false
+      t.int :rental_period, default: 1
+      t.text :description, unique: true, null: false
+      t.int :sidekiq_status, null: false, default: 0
 
       t.timestamps
     end
