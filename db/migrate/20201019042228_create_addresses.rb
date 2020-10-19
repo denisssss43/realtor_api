4fbcbbbec4d3ef8233b45c47be254b64249ecd98
Denis <38821830+denisssss43@users.noreply.github.com>
2020-10-19 12:22:27 +0700

    Update 20201019042228_create_addresses.rb

--- db/migrate/20201019042228_create_addresses.rb
+++ db/migrate/20201019042228_create_addresses.rb
@@ -2,9 +2,9 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.references :city, null: false, foreign_key: true
-      t.string :title
-      t.decimal :latitude, precision: 15, scale: 6
-      t.decimal :longitude, precision: 15, scale: 6
+      t.string :title, null: false
+      t.decimal :latitude, precision: 15, scale: 6, null: false
+      t.decimal :longitude, precision: 15, scale: 6, null: false
 
       t.timestamps
     end
