be4eb248fb6661fbc169652cdde70cb2e0d7af20
Denis <38821830+denisssss43@users.noreply.github.com>
2020-10-19 12:22:23 +0700

    Update 20201019042220_create_cities.rb

--- db/migrate/20201019042220_create_cities.rb
+++ db/migrate/20201019042220_create_cities.rb
@@ -2,7 +2,7 @@ class CreateCities < ActiveRecord::Migration[6.0]
   def change
     create_table :cities do |t|
       t.references :country, null: false, foreign_key: true
-      t.string :title
+      t.string :title, null: false
 
       t.timestamps
     end
