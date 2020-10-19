c69dfc6fd7e97daa181e2b8192e2dfa71848f89d
Denis <38821830+denisssss43@users.noreply.github.com>
2020-10-20 00:24:45 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :offers, only: [:index, :show]
+      # resources :cities, only: [:index, :show]
+      # resources :countries, only: [:index, :show]
+
+      resources :resources 
+
     end
   end
 
